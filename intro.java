38399db273732bbd04a77b0743f391b966be3099
Hruthik08-tech <hruthikesh2008@gmail.com>
2026-05-02 10:19:47 +0530

    First Commit

---
+++ intro.java
@@ -0,0 +1,9 @@
+// We type code file with .java extension, then compile it into new class file with .class file extension 
+// 
+
+public class intro {
+    // main() is a method inside intro class 
+    public static void main (String[] args) {
+        System.out.print("Hello World");
+    }
+}
